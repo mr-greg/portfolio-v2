97995fc0a7a468ebcbf76dc8f8ff065d8dd3abc4
mr-greg <gregory.drv@gmail.com>
2025-01-15 14:03:07 +0100

    adjusting sens for header animation mobile devices

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -7,6 +7,7 @@ export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isVisible, setIsVisible] = useState(true); // Contrôle la visibilité du menu
   const [lastScrollY, setLastScrollY] = useState(0); // Position précédente du scroll
+  const [isScrolling, setIsScrolling] = useState(false); // Indique si un scroll est en cours
   const [buttonText, setButtonText] = useState('Contact');
 
   const handleCopy = async () => {
@@ -46,33 +47,50 @@ export default function Header() {
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
+      const tolerance = 5; // Tolérance pour ignorer les variations mineures
 
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
-      const tolerance = 10; // Ajustez si nécessaire
-  
-      setIsVisible(scrollTop <= tolerance);
-
-      // Assurez-vous que la position a changé de manière significative
-      if (Math.abs(currentScrollY - lastScrollY) > 5) {
-        // Si on scrolle vers le haut, montrer le menu
-        if (currentScrollY <= lastScrollY) {
-          
-          setIsVisible(true);
-        } else {
-          // Si on scrolle vers le bas, cacher le menu
-          setIsVisible(false);
-        }
-        setLastScrollY(currentScrollY); // Met à jour la position précédente
+      // Annule les petits mouvements de scroll (tolérance)
+      if (Math.abs(currentScrollY - lastScrollY) <= tolerance) {
+        return;
       }
+
+      // Vérifie si l'utilisateur scrolle
+      setIsScrolling(true);
+
+      // Si on scrolle vers le haut, montre le menu
+      if (currentScrollY < lastScrollY) {
+        setIsVisible(true);
+      } else {
+        // Si on scrolle vers le bas, cache le menu
+        setIsVisible(false);
+      }
+
+      // Met à jour la position précédente
+      setLastScrollY(currentScrollY);
     };
-  
+
+    const handleScrollEnd = () => {
+      // Détecte la fin de l'inertie sur mobile
+      setTimeout(() => {
+        setIsScrolling(false);
+      }, 100); // Délai pour détecter la fin du scroll
+    };
+
     window.addEventListener('scroll', handleScroll);
-  
+    window.addEventListener('touchend', handleScrollEnd); // Gère la fin du défilement tactile
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('touchend', handleScrollEnd);
     };
   }, [lastScrollY]);
-  
+
+  useEffect(() => {
+    // Force la fermeture du menu si l'utilisateur commence à scroller
+    if (isScrolling) {
+      setIsMenuOpen(false);
+    }
+  }, [isScrolling]);
 
   return (
     <StyledHeader isMenuOpen={isMenuOpen} isVisible={isVisible}>
